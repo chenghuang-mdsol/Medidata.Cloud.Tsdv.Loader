66ae4973ea930be933f8cf9779ded28821ed431c
junshao <jshao@mdsol.com>
2016-02-08 13:37:29 -0500

    Increment package version

--- Medidata.Rave.Tsdv.Loader/Properties/AssemblyInfo.cs
+++ Medidata.Rave.Tsdv.Loader/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.12.0")]
-[assembly: AssemblyFileVersion("0.1.12.0")]
+[assembly: AssemblyVersion("0.1.13.0")]
+[assembly: AssemblyFileVersion("0.1.13.0")]
