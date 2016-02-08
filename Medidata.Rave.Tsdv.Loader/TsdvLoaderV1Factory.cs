bdccd42e0831f50003a28c1a521857c7a35bff65
Cheng Huang <chuang@mdsol.com>
2016-02-08 16:38:02 -0500

    Add AutoCopyrightCoveredResourcedExcelBuilder to add column resource page Update CoverSheet.xlsx template to be more clean and compatible

--- Medidata.Rave.Tsdv.Loader/TsdvLoaderV1Factory.cs
+++ Medidata.Rave.Tsdv.Loader/TsdvLoaderV1Factory.cs
@@ -1,12 +1,14 @@
-﻿using Medidata.Cloud.ExcelLoader;
+﻿using System.Collections.Generic;
+using Medidata.Cloud.ExcelLoader;
 using Medidata.Interfaces.Localization;
+using Medidata.Rave.Tsdv.Loader.ColumnResources;
 using Medidata.Rave.Tsdv.Loader.SheetDefinitions.v1;
 
 namespace Medidata.Rave.Tsdv.Loader
 {
     public class TsdvLoaderV1Factory : TsdvPresentationLoaderFactory
     {
-        public TsdvLoaderV1Factory(ILocalization localization) : base(localization) {}
+        public TsdvLoaderV1Factory(ILocalization localization, IColumnResourceManager resources) : base(localization,resources) {}
 
         protected override IExcelLoader DefineTsdvSheets(TsdvLoaderSupportedVersion version, IExcelLoader loader)
         {
